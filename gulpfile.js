176edb49cead77a10fa176709d32c7df8d5c7667
Vitor Couto <c.svitor@yahoo.com.br>
2018-11-27 01:03:37 -0200

    Refatoring menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    include = require('gulp-file-include');
+
+// Copia arquivos da src para a dist de forma seletiva
+gulp.task('copy', function(){
+    gulp.src(['src/components/**/*', 'src/css/**/*','src/js/**/*', 'src/imagens/**/*'], {"base": "src"})
+    .pipe(gulp.dest('dist'))
+})
+
 
 gulp.task('sass', function(){
     // Origem (src)
@@ -9,18 +17,26 @@ gulp.task('sass', function(){
     .pipe(sass())
 
     // Destino (dest)
-    .pipe(gulp.dest('./src/css/'));
+    .pipe(gulp.dest('./dist/css/'));
 })
+// Atualiza a pasta dist com arquivos da src
+gulp.task('html', function(){
+    gulp.src('./src/index.html')
+    
+    .pipe(include())
 
+    .pipe(gulp.dest('./dist/'));
+})
+// Inicia o gulp server, motirando alterações em todos os arquivos e atualizando automaticamente a dist e o navegador
 gulp.task('server', function(){
     browserSync.init({
         server:{
-            baseDir: 'src'
+            baseDir: 'dist'
         }
     })
 
-    gulp.watch('./src/**/*').on('change', browserSync.reload)
+    gulp.watch('./dist/**/*').on('change', browserSync.reload)
     
     gulp.watch('./src/sass/**/*.scss', ['sass'])
-
+    gulp.watch('./src/**/*.html', ['html'])
 })
